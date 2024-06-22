e8cb3f7365e1d4aa3e60e517678ec3b017591c9e
Ad <me@skissue.xyz>
2024-06-22 08:46:44 -0400

    feat: package templating

---
+++ denote-binary-metadata.el
@@ -0,0 +1,35 @@
+;;; denote-binary-metadata.el --- Store Denote metadata in binary filetypes -*- lexical-binding: t -*-
+
+;; Author: Ad <me@skissue.xyz>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.1") (denote) (exiftool))
+;; Homepage: https://github.com/skissue/denote-binary-metadata
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; (WIP) Store Denote metadata in binary filetypes
+
+;;; Code:
+
+
+
+(provide 'denote-binary-metadata)
+
+;;; denote-binary-metadata.el ends here
